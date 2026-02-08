72ca76012155b32feac2cc6bae2703c30a21e55b
Vivaswannn <vivaswan2002@gmail.com>
2026-02-08 14:53:39 +0530

    Add InheritDemoApp - inheritance and polymorphism demo

---
+++ InheritDemoApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        IPayment payment;
+        payment = new CreditCardPayment();
+        payment.Refund(1000.0);
+        payment.Pay(1000.0);
+    }    //     Employee emp = new Employee;
+    //     EmployeeId = 101;
+    //     Name = "John Doe";
+    //     Salary = "50000";
+    // };
+    // Console.WriteLine("Employee Details:");
+    // Console.WriteLine($"ID: {emp.EmployeeId}, Name: {emp.Name}, Salary: {emp.Salary}");
+    // System.Console.WriteLine($"Employee Object {emp}");
+    
+}
