4292939bb59c9710712afbcc0b649f903513bf07
Vivaswannn <vivaswan2002@gmail.com>
2026-02-08 14:24:36 +0530

    Add EventDelegateDemo project

---
+++ EventDelegateDemoApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+class Program{
+    public static void Main(string[] args)
+    {
+    //     AddTwoNumbers obj = new AddTwoNumbers();
+    //     //subscribe to event
+    //     obj.ev_OddNumber += new AddTwoNumbers.dg_OddNumber(EventMessage);
+    //     // invoke the method to produce output
+    //     obj.Add();
+    // }
+    // //delegate calls this method when event raised
+
+    // static void EventMessage(){
+    //     Console.WriteLine("event executed: odd number found");
+    // }
+
+    Predicate<string> CheckApple - IsApple;
+    bool result= IsApple("I phone x");
+    if(result){
+        Console.WriteLine("It's an apple");
+    }
+    }
+}
