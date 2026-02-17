0322e885999c9cb2138f87dbdc65be76503b44ad
Vivaswannn <vivaswan2002@gmail.com>
2026-02-17 15:02:11 +0530

    Add ConnectedEnvDemo

---
+++ ConnectedEnvDemo/ConnectedEnvDemo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConnectedEnvDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 obj = new Class1();
+            obj.GetAllStudent();
+            Console.ReadLine();
+        }
+    }
+}
