7b866d2e2820d8a1b4fa9ec27283538b38c909a6
Vivaswannn <vivaswan2002@gmail.com>
2026-02-17 15:02:30 +0530

    Add SerializationDemo

---
+++ SerializationDemo/SerializationDemo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerializationDemo
+{
+    internal class Program
+    {
+        public delegate void Print(int value);
+        static void Main(string[] args)
+        {
+            //SerialTest st = new SerialTest();
+            //
+        }
+    }
+}
